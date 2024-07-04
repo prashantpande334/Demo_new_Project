9582aa9c7eee12899189d5f33bcd0120b9124ad1
prashantpande334 <160888692+prashantpande334@users.noreply.github.com>
2024-07-04 17:21:29 +0530

    Add files via upload

---
+++ mySql new.sql
@@ -0,0 +1,9 @@
+create database test25;
+use test25;
+create table employee( id int not null primary key auto_increment,firstName varchar(100), lastName varchar(100),
+ address varchar(100),
+city varchar(100),salary varchar(100));
+select * from students;
+
+
+
